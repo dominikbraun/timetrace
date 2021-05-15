18997b0c16a22e33e5e6aa094f6223f7f54335bd
Dominik Braun <mail@dominikbraun.io>
2021-05-15 15:12:46 +0200

    Implement list records command

--- cli/list.go
+++ cli/list.go
@@ -2,11 +2,16 @@ package cli
 
 import (
 	"strconv"
+	"time"
 
 	"github.com/dominikbraun/timetrace/core"
 	"github.com/dominikbraun/timetrace/out"
 
 	"github.com/spf13/cobra"
+)
+
+const (
+	defaultTimeLayout = "15:04"
 )
 
 func listCommand(t *core.Timetrace) *cobra.Command {
@@ -19,6 +24,7 @@ func listCommand(t *core.Timetrace) *cobra.Command {
 	}
 
 	list.AddCommand(listProjectsCommand(t))
+	list.AddCommand(listRecordsCommand(t))
 
 	return list
 }
@@ -47,4 +53,57 @@ func listProjectsCommand(t *core.Timetrace) *cobra.Command {
 	}
 
 	return listProjects
+}
+
+func listRecordsCommand(t *core.Timetrace) *cobra.Command {
+	listRecords := &cobra.Command{
+		Use:   "records <YYYY-MM-DD>",
+		Short: "List all records from a date",
+		Args:  cobra.ExactArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			date, err := time.Parse("2006-01-02", args[0])
+			if err != nil {
+				out.Err("failed to parse date: %s", err.Error())
+				return
+			}
+
+			records, err := t.ListRecords(date)
+			if err != nil {
+				out.Err("failed to list records: %s", err.Error())
+				return
+			}
+
+			dateLayout := defaultTimeLayout
+
+			if t.Config().Use12Hours {
+				dateLayout = "03:04PM"
+			}
+
+			rows := make([][]string, len(records))
+
+			for i, record := range records {
+				end := defaultString
+
+				if record.End != nil {
+					end = record.End.Format(dateLayout)
+				}
+
+				billable := defaultBool
+
+				if record.IsBillable {
+					billable = "yes"
+				}
+
+				rows[i] = make([]string, 4)
+				rows[i][0] = strconv.Itoa(i + 1)
+				rows[i][1] = record.Start.Format(dateLayout)
+				rows[i][2] = end
+				rows[i][3] = billable
+			}
+
+			out.Table([]string{"#", "Start", "End", "Billable"}, rows)
+		},
+	}
+
+	return listRecords
 }
