7c255f5046d68e91ef6b8ac0d0d4d05b163c6411
rknizzle <ryan@autotiv.com>
2021-05-16 12:29:18 -0400

    Implement delete record command powered by Timetrace.DeleteRecord

--- cli/delete.go
+++ cli/delete.go
@@ -1,6 +1,8 @@
 package cli
 
 import (
+	"time"
+
 	"github.com/dominikbraun/timetrace/core"
 	"github.com/dominikbraun/timetrace/out"
 
@@ -17,6 +19,7 @@ func deleteCommand(t *core.Timetrace) *cobra.Command {
 	}
 
 	delete.AddCommand(deleteProjectCommand(t))
+	delete.AddCommand(deleteRecordCommand(t))
 
 	return delete
 }
@@ -43,4 +46,40 @@ func deleteProjectCommand(t *core.Timetrace) *cobra.Command {
 	}
 
 	return deleteProject
+}
+
+func deleteRecordCommand(t *core.Timetrace) *cobra.Command {
+	deleteRecord := &cobra.Command{
+		Use:   "record YYYY-MM-DD-HH-MM",
+		Short: "Delete a record",
+		Args:  cobra.ExactArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			layout := defaultRecordArgLayout
+
+			if t.Config().Use12Hours {
+				layout = "2006-01-02-03-04PM"
+			}
+
+			start, err := time.Parse(layout, args[0])
+			if err != nil {
+				out.Err("Failed to parse date argument: %s", err.Error())
+				return
+			}
+
+			record, err := t.LoadRecord(start)
+			if err != nil {
+				out.Err("Failed to read record: %s", err.Error())
+				return
+			}
+
+			if err := t.DeleteRecord(*record); err != nil {
+				out.Err("Failed to delete %s", err.Error())
+				return
+			}
+
+			out.Success("Deleted record %s", args[0])
+		},
+	}
+
+	return deleteRecord
 }
