d3cb2cee32e1755e35d364275951d2b868c69176
KonstantinGasser <konstantinfranzkorbinian.gasser@stud.hs-mannheim.de>
2021-06-09 17:22:15 +0200

    changed pretty format func in formatter to be more simple

--- cli/report.go
+++ cli/report.go
@@ -15,34 +15,31 @@ type reportOptions struct {
 	projectKey   string
 	outputFormat string
 	outputPath   string
-	fromTime     string
-	toTime       string
+	startTime    string
+	endTime      string
 }
 
 func generateReportCommand(t *core.Timetrace) *cobra.Command {
 	var options reportOptions
 
 	report := &cobra.Command{
-		Use:   "report",
-		Short: "Report allows to view or output tracked records as defined report",
+		Use:    "report",
+		Short:  "Report allows to view or output tracked records as defined report",
+		Hidden: true,
 		Run: func(cmd *cobra.Command, args []string) {
-			var fromDate, toDate time.Time
+			var startDate, endDate time.Time
 			var formatErr error
 
-			// TODO: find better way to check if flag is default
-			// matching the default string will be buggy
-			if options.fromTime != "from oldest" {
-				fromDate, formatErr = t.Formatter().ParseDate(options.fromTime)
+			if options.startTime != "" {
+				startDate, formatErr = t.Formatter().ParseDate(options.startTime)
 				if formatErr != nil {
 					out.Err("failed to parse date: %s", formatErr.Error())
 					return
 				}
 			}
 
-			// TODO: find better way to check if flag is default
-			// matching the default string will be buggy
-			if options.toTime != "to newest" {
-				toDate, formatErr = t.Formatter().ParseDate(options.toTime)
+			if options.endTime != "" {
+				endDate, formatErr = t.Formatter().ParseDate(options.endTime)
 				if formatErr != nil {
 					out.Err("failed to parse date: %s", formatErr.Error())
 					return
@@ -54,11 +51,10 @@ func generateReportCommand(t *core.Timetrace) *cobra.Command {
 				// this will ignore records which end time to not set
 				// so current tracked times for example
 				core.FilterNoneNilEndTime,
-				core.FilterByTimeRange(fromDate, toDate),
+				core.FilterByTimeRange(startDate, endDate),
 			}
-			// TODO: find better way to check if flag is default
-			// matching the default string will be buggy
-			if options.projectKey != "include all projects" {
+
+			if options.projectKey != "" {
 				filter = append(filter, core.FilterByProject(options.projectKey))
 			}
 			if options.isBillable {
@@ -100,14 +96,14 @@ func generateReportCommand(t *core.Timetrace) *cobra.Command {
 	report.Flags().BoolVarP(&options.isBillable, "billable", "b",
 		false, "filter for only billable records")
 
-	report.Flags().StringVarP(&options.fromTime, "start", "s",
-		"from oldest", "filter records from a given start date <YYYY-MM-DD>")
+	report.Flags().StringVarP(&options.startTime, "start", "s",
+		"", "filter records from a given start date <YYYY-MM-DD>")
 
-	report.Flags().StringVarP(&options.toTime, "end", "e",
-		"to newest", "filter records to a given end date (end is inclusive) <YYYY-MM-DD>")
+	report.Flags().StringVarP(&options.endTime, "end", "e",
+		"", "filter records to a given end date (end is inclusive) <YYYY-MM-DD>")
 
 	report.Flags().StringVarP(&options.projectKey, "project", "p",
-		"include all projects", "filter records by a specific project")
+		"", "filter records by a specific project")
 
 	report.Flags().StringVarP(&options.outputFormat, "format", "f",
 		"print table", "output format for report (json/csv)")
