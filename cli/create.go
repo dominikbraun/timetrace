414a900c8ec4761f5816548869c0e3e208413773
Felx Theodor <felix.theodor@rub.de>
2021-06-21 13:03:57 +0200

    add spaces and exactArgs to new command

--- cli/create.go
+++ cli/create.go
@@ -49,8 +49,9 @@ func createProjectCommand(t *core.Timetrace) *cobra.Command {
 func createRecordCommand(t *core.Timetrace) *cobra.Command {
 	var options startOptions
 	createRecord := &cobra.Command{
-		Use:   "record <PROJECT KEY>{<YYYY-MM-DD>|today|yesterday}<HH-MM><HH-MM>",
+		Use:   "record <PROJECT KEY> {<YYYY-MM-DD>|today|yesterday} <HH-MM> <HH-MM>",
 		Short: "Create a new record",
+		Args:  cobra.ExactArgs(4),
 		Run: func(cmd *cobra.Command, args []string) {
 			key := args[0]
 			project, err := t.LoadProject(key)
