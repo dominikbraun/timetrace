1f70ecd28ff40f98a6824b72541017507805e3f2
Felx Theodor <felix.theodor@rub.de>
2021-06-22 14:07:47 +0200

    add unit-tests for collide method

--- core/timetrace_test.go
+++ core/timetrace_test.go
@@ -5,6 +5,59 @@ import (
 	"time"
 )
 
+func newTestRecord(s int, e int) Record {
+	start := time.Now().Add(time.Duration(s) * time.Minute)
+	end := time.Now().Add(time.Duration(e) * time.Minute)
+	return Record{Start: start, End: &end}
+}
+
+func TestCollides(t *testing.T) {
+	savedRec := newTestRecord(-60, -1)
+	allRecs := []*Record{&savedRec}
+
+	// rec1 starts and end after savedRec
+	rec1 := newTestRecord(-1, 0)
+
+	if collides(rec1, allRecs) {
+		t.Error("records should not collide")
+	}
+
+	// rec2 starts in savedRec, ends after
+	rec2 := newTestRecord(-30, 1)
+
+	if !collides(rec2, allRecs) {
+		t.Error("records should collide")
+	}
+
+	// rec3 start before savedRec, ends inside
+	rec3 := newTestRecord(-75, -30)
+
+	if !collides(rec3, allRecs) {
+		t.Error("records should collide")
+	}
+
+	// rec4 starts and ends before savedRec
+	rec4 := newTestRecord(-75, -70)
+
+	if collides(rec4, allRecs) {
+		t.Error("records should not collide")
+	}
+
+	// rec5 starts and ends inside savedRec
+	rec5 := newTestRecord(-40, -20)
+
+	if !collides(rec5, allRecs) {
+		t.Error("records should collide")
+	}
+
+	// rec6 starts before and ends after savedRec
+	rec6 := newTestRecord(-70, 10)
+
+	if !collides(rec6, allRecs) {
+		t.Error("records should collide")
+	}
+}
+
 func TestFormatDuration(t *testing.T) {
 
 	tt := []struct {
