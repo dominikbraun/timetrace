8f37e1afc45b4943b1307dee055c6d6e6841793a
Charles Winter <charleswinter27@gmail.com>
2021-07-07 21:05:22 +0100

    Adds test to upload record to JIRA method to ensure error is returned

--- integrations/jira/upload_record_test.go
+++ integrations/jira/upload_record_test.go
@@ -59,7 +59,7 @@ func TestUploadRecord(t *testing.T) {
 		roundTripper := roundTripFunc(func(r *http.Request) (*http.Response, error) {
 			defer func() { i++ }()
 			checkRequestsEqual(r, expectedRequests[i], t)
-
+			// TODO: check that the body in the request is as expected
 			return responses[i], nil
 		})
 
@@ -69,6 +69,35 @@ func TestUploadRecord(t *testing.T) {
 
 		if err != nil {
 			t.Fatal(err)
+		}
+	})
+	t.Run("it returns an error on non 201 from JIRA when creating worklog", func(t *testing.T) {
+		var (
+			// started 8 hours ago, finished 5 minutes ago.
+			endTime    = time.Now().Add(-time.Minute * 5)
+			startTime  = endTime.Add(-time.Hour * 8)
+			projectKey = "project.key"
+			record     = &core.Record{
+				Start: startTime,
+				End:   &endTime,
+				Project: &core.Project{
+					Key: projectKey,
+				},
+			}
+		)
+
+		roundTripper := roundTripFunc(func(r *http.Request) (*http.Response, error) {
+			return &http.Response{
+				StatusCode: http.StatusUnauthorized,
+			}, nil
+		})
+
+		err := jira.New(jira.RepositoryConfig{
+			HTTPClient: &http.Client{Transport: roundTripper},
+		}).UploadRecord(record)
+
+		if err == nil {
+			t.Fatal("wanted an error but didn't get one")
 		}
 	})
 }
