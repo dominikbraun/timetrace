3e614e2aaa0bc5ca9ad99e387c93510d46c4d59e
Romain Le Toulouse <romain@invodia.com>
2022-08-14 14:03:37 +0300

    Report duration to round to closest minute

--- core/formatter.go
+++ core/formatter.go
@@ -52,7 +52,7 @@ const (
 	defaultTimeLayout        = "15:04"
 	default12HoursTimeLayout = "03:04PM"
 	defaultDatelayoutPretty  = "Monday, 02. January 2006"
-	defaultDurationLayout	 = time.Second
+	defaultDurationLayout	 = time.Minute
 )
 
 func (f *Formatter) timeLayout() string {
@@ -71,7 +71,7 @@ func (f *Formatter) TimeString(input time.Time) string {
 }
 
 func (f *Formatter) DurationString(input time.Duration) string {
-	return input.Truncate(f.durationLayout()).String()
+	return input.Round(f.durationLayout()).String()
 }
 
 // PrettyDateString returns a nice representation of a given time
